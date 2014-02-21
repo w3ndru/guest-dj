372d88bc886c817dd69a2f820535a682097fa0dd
Andrew Wen <awen@zendesk.com>
2014-02-21 10:20:24 -0800

    Added collections for requests and profiles, added default route to /home

--- client/js/application.js
+++ client/js/application.js
@@ -1,30 +1,30 @@
 if(Meteor.isClient) {
   GD = new Object({
-    djs: new Meteor.Collection("djs"),
+    djs: new Meteor.Collection('djs'),
     events: new Meteor.Collection('events'),
     playlists: new Meteor.Collection('playlists'),
+    profiles: new Meteor.Collection('profiles'),
+    requests: new Meteor.Collection('requests'),
+
     currentPlaylistUrl: function() {
       var playlist = Session.get('currentPlaylist');
-
       if(!playlist) { return ''; }
-
-      return window.location.origin +
-              '/playlist/' +
-              playlist._id;
+      return window.location.origin + '/playlist/' + playlist._id;
     },
+
     currentPlaylistQRUrl: function() {
       var playlist = Session.get('currentPlaylist');
       if(!playlist) { return ''; }
-      return "https://chart.googleapis.com/chart?chs=120x120&cht=qr&chl=" +
-              window.location.origin +
-              '/playlist/' +
-              playlist._id;
+      return 'https://chart.googleapis.com/chart?chs=120x120&cht=qr&chl=' +
+              window.location.origin + '/playlist/' + playlist._id;
     }
   });
 
-  Meteor.subscribe("djs");
-  Meteor.subscribe("events");
-  Meteor.subscribe("playlists");
+  Meteor.subscribe('djs');
+  Meteor.subscribe('events');
+  Meteor.subscribe('playlists');
+  Meteor.subscribe('profiles');
+  Meteor.subscribe('requests');
 
   Meteor.startup(function() {
     Session.set('currentEvent', null);
@@ -61,5 +61,10 @@ Meteor.Router.add({
   '/playlist/:id': function(id) {
     GD.displayListId = id;
     return 'playlist';
+  },
+
+  '*': function() {
+    Session.set('currentSection', 'home');
+    return 'application';
   }
 });
--- server/js/application.js
+++ server/js/application.js
@@ -1,12 +1,16 @@
 if(Meteor.isServer) {
   Meteor.startup(function () {
-    DJs = new Meteor.Collection("djs");
-    Events = new Meteor.Collection('events');
-    Playlists = new Meteor.Collection('playlists');
+    DJs = new Meteor.Collection("djs"); // {_id, userID}
+    Events = new Meteor.Collection('events'); // {_id, name, playlistId}
+    Playlists = new Meteor.Collection('playlists'); // {_id, userId, even, tracks}, track: [{artist, title}]
+    Profiles = new Meteor.Collection('profiles');
+    Requests = new Meteor.Collection('requests');
 
     Meteor.publish('djs', function() { return DJs.find(); });
     Meteor.publish('events', function() { return Events.find(); });
     Meteor.publish('playlists', function() { return Playlists.find(); });
+    Meteor.publish('profiles', function() { return Profiles.find(); });
+    Meteor.publish('requests', function() { return Requests.find(); });
 
     DJs.allow({
       insert: function (userId, doc) { return true; },
@@ -24,7 +28,19 @@ if(Meteor.isServer) {
       insert: function (userId, doc) { return true; },
       remove: function (userId, doc) { return true; },
       fetch: ['owner']
-    })
+    });
+
+    Profiles.allow({
+      insert: function (userId, doc) { return true; },
+      remove: function (userId, doc) { return true; },
+      fetch: ['owner']
+    });
+
+    Requests.allow({
+      insert: function (userId, doc) { return true; },
+      remove: function (userId, doc) { return true; },
+      fetch: ['owner']
+    });
 
   });
 }
